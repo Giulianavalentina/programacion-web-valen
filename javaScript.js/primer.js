579cb7ea7a55e1869eed745a4d64b27a1016807f
Giulianavalentina <Valu0620@gmail.com>
2024-04-22 16:57:56 -0300

    tengo que seguir con los demas pokemon

---
+++ javaScript.js/primer.js
@@ -0,0 +1,17 @@
+var numero = 1; //asignamos el valor a una variable tipo number
+var palabra = "palabra"; //asignamos el valor a una variable tipo string
+
+//definimos variables con la palabra reservada let para evitar el llamado de las mismas desde cualquier parte del codigo.
+let numero2 = 2;
+let palabra2 = "palabra 2";
+
+//definimos constantes con la palabra reservada const
+const numConstante = 4; //siempre que sepan que no van a mutar o modificar un valor usan CONST
+
+//ejemplo:
+
+if (numero > 0) {
+  let numero3 = 3;
+}
+
+console.log("numero", numero2); //mostrar un valor por pantalla
