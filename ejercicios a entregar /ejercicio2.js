b87f556bbb238b5b5293a5997a11f7333512d550
Giulianavalentina <Valu0620@gmail.com>
2024-05-27 17:08:49 -0300

    Casi por terminar el 2

---
+++ ejercicios a entregar /ejercicio2.js
@@ -0,0 +1,16 @@
+function AgregarElementos() {
+    let lista = document.getElementById("Lista");
+    let input = document.getElementById("textarea").value;
+    const elementoLista = document.createElement("li");
+
+    const button = document.createElement("button");
+    button.textContent = "delete";
+
+    elementoLista.textContent = input 
+    elementoLista.appendChild(button);
+
+    lista.appendChild(elementoLista);
+    button.onclick = function(){
+        lista.removeChild(elementoLista);
+    }
+}
