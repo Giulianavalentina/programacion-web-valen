39862cbd02d4fbbacdb1a89cf31a04ca7f8f8d58
Giulianavalentina <Valu0620@gmail.com>
2024-06-03 17:06:50 -0300

    Me quede en agregar la foto

---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import MiPrimerComponente from "./components/MiPrimerComponente"
+function App() {
+
+  return (
+    <> Hola! Soy Valen
+    </>
+  )
+}
+
+export default App
---
+++ react/vite-project/src/components/MiPrimerComponente.jsx
@@ -0,0 +1,8 @@
+export function MiPrimerComponente({name, lastname}) {
+    return (
+        <>
+        Mi nombre es {name} {lastname}
+        <img src="."></>
+        </>
+    )
+}
